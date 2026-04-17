ab219fe827f5304746fcf090ef9690bab24694f9
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-04-18 00:12:19 +0800

    chore: update projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,12 +3,14 @@ export const projects = [
     title: "Academic Calendar & Document Request",
     description:
       "A web application built with React and Tailwind CSS that allows students to view their academic calendar and request documents such as transcripts and certificates.",
-    link: "#",
+    link: "https://calendar-vmc.pages.dev/",
+    github: "https://github.com/eustaquiogalano/calendar-vmc",
   },
   {
     title: "Shopping Cart",
     description:
       "A simple shopping cart application built with React. It allows users to add items to their cart, view the cart, and proceed to checkout.",
     link: "#",
+    github: "https://github.com/eustaquiogalano/shopping-cart-odin",
   },
 ];
