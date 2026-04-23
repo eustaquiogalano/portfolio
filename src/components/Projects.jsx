b5b9513d2705b7a0d6a5184a3a7b6df9797d9998
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-04-23 13:28:48 +0800

    refactor: update project image links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -29,11 +29,11 @@ function Projects() {
               <CarouselItem className="sm:basis-1/2 ">
                 <div className="p-1">
                   <Card className="relative mx-auto w-full max-w-sm pt-0">
-                    <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+                    {/* <div className="absolute inset-0 z-30 aspect-video bg-black/35" /> */}
                     <img
-                      src="https://avatar.vercel.sh/shadcn1"
+                      src={project.img}
                       alt="Event cover"
-                      className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+                      className="relative aspect-video w-full object-cover "
                     />
                     <CardHeader className="flex flex-col gap-[1rem]">
                       <CardTitle>{project.title}</CardTitle>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,7 @@
 export const projects = [
   {
     title: "Academic Calendar & Document Request",
+    img: "src/assets/images/calendar_sapshot.png",
     description:
       "A web application built with React and Tailwind CSS that allows students to view their academic calendar and request documents such as transcripts and certificates.",
     link: "https://calendar-vmc.pages.dev/",
@@ -8,9 +9,11 @@ export const projects = [
   },
   {
     title: "Shopping Cart",
+    img: "src/assets/images/lap-shop.png",
+
     description:
       "A simple shopping cart application built with React. It allows users to add items to their cart, view the cart, and proceed to checkout.",
-    link: "#",
+    link: "https://shopping-cart-odin.pages.dev/",
     github: "https://github.com/eustaquiogalano/shopping-cart-odin",
   },
 ];
